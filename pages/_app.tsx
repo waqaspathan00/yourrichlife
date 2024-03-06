92f9032a90b6a43e2f41a9157bec13a763f36cd6
waqasabbasi <waqaspathan00@gmail.com>
2024-03-06 00:39:05 -0500

    disable double tap to zoom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,10 @@ export default function App({Component, pageProps}: AppProps) {
         <SavingsDataProvider>
             <ModalOpenProvider>
                 <Toaster/>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+                />
 
                 <Component {...pageProps} />
             </ModalOpenProvider>
